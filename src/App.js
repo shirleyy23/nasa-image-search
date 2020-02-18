c93f34588439a3a8c0e8ac1fc566368106c22031
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-02-18 13:19:49 -0500

    Modified App.css file and imported ImageContext - Added react strict mode to app - included reactdom - imported ImageContext - created State with useState hook to be used in ImageContext - Added useEffect to load correct and reset data when app mounts and unmounts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
+import ImageContext from './components/ImageContext/ImageContext';
 
 function App() {
-  return <div className="App" />;
+  const [results, setResults] = useState([]);
+  useEffect(() => {
+    setResults([]);
+    return () => setResults([]);
+  }, []);
+  return (
+    <React.StrictMode>
+      <ImageContext.Provider
+        value={{
+          results,
+          changeResults: e => setResults(e),
+        }}
+      >
+        <div className="App" />
+      </ImageContext.Provider>
+    </React.StrictMode>
+  );
 }
 
 export default App;
