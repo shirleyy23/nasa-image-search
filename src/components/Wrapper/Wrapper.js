6a47839c28960abd10f4a8106b84a6f6b52a85fb
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-02-24 00:03:40 -0500

    updated values in useEffect array

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -76,7 +76,7 @@ const Wrapper = () => {
       );
     });
     return () => setBackground({});
-  }, []);
+  }, [link, title, copyright, setBackground]);
 
   return (
     <StyledWrapper style={wrapperBackground}>
