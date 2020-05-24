1bcb775cebfcedca56de533b29fe9598eb7e3f1b
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-24 10:51:49 -0400

    removed unnecessary import ReactDOM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import ImageContext from './components/ImageContext/ImageContext';
